753fc7a964a4c5238b6bdad3872af2e5441779b6
Vladislav Kantaev <vvkantaev@yandex.ru>
2020-09-04 22:49:19 +0300

    Remove redundant files

--- FixedStack.cs
+++ FixedStack.cs
@@ -1,197 +1,192 @@
 ﻿using System;
 using System.Collections;
 using System.Collections.Generic;
-using JetBrains.Annotations;
 
 namespace DataStructures
 {
-    public class FixedStack<T> : ICollection, IEnumerable<T>
-    {
-        #region Private fields
+	public class FixedStack<T> : ICollection, IEnumerable<T>
+	{
+		#region Private fields
 
-        [NotNull, ItemCanBeNull] public readonly T[] InternalArray;
+		private readonly T[] _internalArray;
 
-        #endregion
+		#endregion
 
-        #region Constructors
+		#region Constructors
 
-        public FixedStack(int maxSize)
-        {
-            if (maxSize < 0) throw new ArgumentOutOfRangeException(nameof(maxSize), "Maximum size cannot be negative.");
-            InternalArray = new T[maxSize];
-        }
+		public FixedStack(int maxSize)
+		{
+			if (maxSize < 0) throw new ArgumentOutOfRangeException(nameof(maxSize), "Maximum size cannot be negative.");
+			_internalArray = new T[maxSize];
+		}
 
-        public FixedStack(long maxSize)
-        {
-            if (maxSize < 0L) throw new ArgumentOutOfRangeException(nameof(maxSize), "Maximum size cannot be negative.");
-            InternalArray = new T[maxSize];
-        }
+		public FixedStack(long maxSize)
+		{
+			if (maxSize < 0L)
+				throw new ArgumentOutOfRangeException(nameof(maxSize), "Maximum size cannot be negative.");
+			_internalArray = new T[maxSize];
+		}
 
-        #endregion
+		#endregion
 
-        #region Public properties
+		#region Public properties
 
-        /// <summary>
-        /// Stores the current number of elements inside stack.
-        /// </summary>
-        /// <exception cref="InvalidOperationException"></exception>
-        public int Count {
-            get {
-                if (LongCount > int.MaxValue)
-                {
-                    throw new InvalidOperationException("Count value does not fit inside Int32 type.");
-                }
+		/// <summary>
+		/// Stores the current number of elements inside stack.
+		/// </summary>
+		/// <exception cref="InvalidOperationException"></exception>
+		public int Count
+		{
+			get
+			{
+				if (LongCount > int.MaxValue)
+					throw new InvalidOperationException("Count value does not fit inside Int32 type.");
 
-                return (int)LongCount;
-            }
-        }
+				return (int) LongCount;
+			}
+		}
 
-        /// <summary>
-        /// Stores the current number of elements inside stack.
-        /// </summary>
-        public long LongCount { get; private set; }
+		/// <summary>
+		/// Stores the current number of elements inside stack.
+		/// </summary>
+		public long LongCount { get; private set; }
 
-        /// <summary>
-        /// Maximum possible size of the stack.
-        /// </summary>
-        /// <exception cref="InvalidOperationException"></exception>
-        public int MaxSize {
-            get {
-                if (InternalArray.LongLength > int.MaxValue)
-                {
-                    throw new InvalidOperationException("Count value does not fit inside Int32 type.");
-                }
+		/// <summary>
+		/// Maximum possible size of the stack.
+		/// </summary>
+		/// <exception cref="InvalidOperationException"></exception>
+		public int MaxSize
+		{
+			get
+			{
+				if (_internalArray.LongLength > int.MaxValue)
+					throw new InvalidOperationException("Count value does not fit inside Int32 type.");
 
-                return InternalArray.Length;
-            }
-        }
+				return _internalArray.Length;
+			}
+		}
 
-        /// <summary>
-        /// Maximum possible size of the stack.
-        /// </summary>
-        public long MaxLongSize => InternalArray.LongLength;
+		/// <summary>
+		/// Maximum possible size of the stack.
+		/// </summary>
+		public long MaxLongSize => _internalArray.LongLength;
 
-        /// <summary>
-        /// Identifies whether stack achieved its maximum size/
-        /// </summary>
-        public bool IsFull => LongCount == InternalArray.LongLength;
+		/// <summary>
+		/// Identifies whether stack achieved its maximum size/
+		/// </summary>
+		public bool IsFull => LongCount == _internalArray.LongLength;
 
-        #endregion
+		#endregion
 
-        #region Public methods
+		#region Public methods
 
-        /// <summary>
-        /// Push an item to the top of the stack.
-        /// </summary>
-        /// <param name="item">Item to push.</param>
-        /// <exception cref="InvalidOperationException"></exception>
-        public void Push([CanBeNull] T item)
-        {
-            if (IsFull) throw new InvalidOperationException("Stack is full.");
-            InternalArray[LongCount++] = item;
-        }
+		/// <summary>
+		/// Push an item to the top of the stack.
+		/// </summary>
+		/// <param name="item">Item to push.</param>
+		/// <exception cref="InvalidOperationException"></exception>
+		public void Push(T item)
+		{
+			if (IsFull) throw new InvalidOperationException("Stack is full.");
+			_internalArray[LongCount++] = item;
+		}
 
-        /// <summary>
-        /// Remove an item from the top of the stack and return the removed item.
-        /// </summary>
-        /// <returns>Removed item.</returns>
-        /// <exception cref="InvalidOperationException"></exception>
-        [CanBeNull]
-        public T Pop()
-        {
-            if (LongCount == 0L) throw new InvalidOperationException("Stack is empty.");
-            LongCount--;
-            var popped = InternalArray[LongCount];
-            InternalArray[LongCount] = default;
-            return popped;
-        }
+		/// <summary>
+		/// Remove an item from the top of the stack and return the removed item.
+		/// </summary>
+		/// <returns>Removed item.</returns>
+		/// <exception cref="InvalidOperationException"></exception>
+		public T Pop()
+		{
+			if (LongCount == 0L) throw new InvalidOperationException("Stack is empty.");
+			LongCount--;
+			var popped = _internalArray[LongCount];
+			_internalArray[LongCount] = default;
+			return popped;
+		}
 
-        /// <summary>
-        /// Look at the top item.
-        /// </summary>
-        /// <returns>Item on top of the stack.</returns>
-        /// <exception cref="InvalidOperationException"></exception>
-        [CanBeNull]
-        public T Peek()
-        {
-            if (LongCount == 0) throw new InvalidOperationException("Stack is empty.");
-            return InternalArray[LongCount - 1];
-        }
+		/// <summary>
+		/// Look at the top item.
+		/// </summary>
+		/// <returns>Item on top of the stack.</returns>
+		/// <exception cref="InvalidOperationException"></exception>
+		public T Peek()
+		{
+			if (LongCount == 0) throw new InvalidOperationException("Stack is empty.");
+			return _internalArray[LongCount - 1];
+		}
 
-        /// <summary>
-        /// Remove all items from the stack.
-        /// </summary>
-        public void Clear()
-        {
-            for (var i = 0L; i < LongCount; i++)
-            {
-                InternalArray[i] = default;
-            }
+		/// <summary>
+		/// Remove all items from the stack.
+		/// </summary>
+		public void Clear()
+		{
+			for (var i = 0L; i < LongCount; i++)
+			{
+				_internalArray[i] = default;
+			}
 
-            LongCount = 0;
-        }
+			LongCount = 0;
+		}
 
-        #endregion
+		#endregion
 
-        #region ICollection members
+		#region ICollection members
 
-        /// <summary>
-        /// Copy items from stack to the given array.
-        /// </summary>
-        /// <param name="array">Array to copy items to.</param>
-        /// <param name="index">Start index.</param>
-        /// <exception cref="ArgumentNullException"></exception>
-        public void CopyTo([NotNull, ItemCanBeNull] T[] array, int index)
-        {
-            if (array is null) throw new ArgumentNullException(nameof(array));
+		/// <summary>
+		/// Copy items from stack to the given array.
+		/// </summary>
+		/// <param name="array">Array to copy items to.</param>
+		/// <param name="index">Start index.</param>
+		/// <exception cref="ArgumentNullException"></exception>
+		public void CopyTo(T[] array, int index)
+		{
+			if (array is null) throw new ArgumentNullException(nameof(array));
 
-            for (var i = 0L; i < LongCount && i + index < array.LongLength; i++)
-            {
-                array[i + index] = InternalArray[i];
-            }
-        }
+			for (var i = 0L; i < LongCount && i + index < array.LongLength; i++)
+			{
+				array[i + index] = _internalArray[i];
+			}
+		}
 
-        void ICollection.CopyTo([ItemCanBeNull] Array array, int index)
-        {
-            if (array is null) throw new ArgumentNullException(nameof(array));
+		void ICollection.CopyTo(Array array, int index)
+		{
+			if (array is null) throw new ArgumentNullException(nameof(array));
 
-            try
-            {
-                for (var i = 0L; i < LongCount && i + index < array.LongLength; i++)
-                {
-                    array.SetValue(InternalArray[i], i + index);
-                }
-            }
-            catch (ArgumentException)
-            {
-                throw new ArgumentException("The given array has more than one dimension.");
-            }
-            catch (InvalidCastException)
-            {
-                throw new InvalidCastException($"{typeof(T)} is not compatible with type of the given array.");
-            }
-        }
+			try
+			{
+				for (var i = 0L; i < LongCount && i + index < array.LongLength; i++)
+				{
+					array.SetValue(_internalArray[i], i + index);
+				}
+			}
+			catch (ArgumentException)
+			{
+				throw new ArgumentException("The given array has more than one dimension.");
+			}
+			catch (InvalidCastException)
+			{
+				throw new InvalidCastException($"{typeof(T)} is not compatible with type of the given array.");
+			}
+		}
 
-        public IEnumerator<T> GetEnumerator()
-        {
-            for (var i = LongCount - 1; i >= 0L; i--)
-            {
-                yield return InternalArray[i];
-            }
-        }
+		public IEnumerator<T> GetEnumerator()
+		{
+			for (var i = LongCount - 1; i >= 0L; i--)
+			{
+				yield return _internalArray[i];
+			}
+		}
 
-        bool ICollection.IsSynchronized => true;
+		bool ICollection.IsSynchronized => true;
 
-        /// <summary>
-        /// Thread synchronization root.
-        /// </summary>
-        public object SyncRoot { get; } = new object();
+		/// <summary>
+		/// Thread synchronization root.
+		/// </summary>
+		public object SyncRoot { get; } = new object();
 
-        IEnumerator IEnumerable.GetEnumerator()
-        {
-            return GetEnumerator();
-        }
+		IEnumerator IEnumerable.GetEnumerator() => GetEnumerator();
 
-        #endregion
-    }
+		#endregion
+	}
 }
--- MinHeap.cs
+++ MinHeap.cs
@@ -1,216 +1,204 @@
 ﻿using System;
 using System.Collections;
 using System.Collections.Generic;
-using JetBrains.Annotations;
 
 namespace DataStructures
 {
-    /// <summary>
-    /// Binary heap having minimum element at the root position.
-    /// </summary>
-    /// <typeparam name="T">Type of items in heap.</typeparam>
-    public class MinHeap<T> : IPriorityQueue<T> where T : IComparable<T>
-    {
-        #region Contructors
+	/// <summary>
+	/// Binary heap having minimum element at the root position.
+	/// </summary>
+	/// <typeparam name="T">Type of items in heap.</typeparam>
+	public class MinHeap<T> : IPriorityQueue<T> where T : IComparable<T>
+	{
+		#region Contructors
 
-        /// <summary>
-        /// Create a new Min Heap specifying initial capacity.
-        /// </summary>
-        /// <param name="capacity">Initial size of the internal array.</param>
-        /// <exception cref="ArgumentOutOfRangeException"></exception>
-        public MinHeap(int capacity)
-        {
-            if (capacity <= 0) throw new ArgumentOutOfRangeException(nameof(capacity));
-            _list = new List<T>(capacity);
-        }
+		/// <summary>
+		/// Create a new Min Heap specifying initial capacity.
+		/// </summary>
+		/// <param name="capacity">Initial size of the internal array.</param>
+		/// <exception cref="ArgumentOutOfRangeException"></exception>
+		public MinHeap(int capacity)
+		{
+			if (capacity <= 0) throw new ArgumentOutOfRangeException(nameof(capacity));
+			_list = new List<T>(capacity);
+		}
 
-        /// <summary>
-        /// Create a new Min Heap.
-        /// </summary>
-        public MinHeap()
-        {
-            _list = new List<T>();
-        }
+		/// <summary>
+		/// Create a new Min Heap.
+		/// </summary>
+		public MinHeap() => _list = new List<T>();
 
-        #endregion
+		#endregion
 
-        #region Private fields
+		#region Private fields
 
-        /// <summary>
-        /// List containing all items in the heap.
-        /// </summary>
-        [NotNull, ItemNotNull] private readonly List<T> _list;
+		/// <summary>
+		/// List containing all items in the heap.
+		/// </summary>
+		private readonly List<T> _list;
 
-        #endregion
+		#endregion
 
-        #region Private properties
+		#region Private properties
 
-        /// <summary>
-        /// Get the index of the parent of a particular node.
-        /// </summary>
-        /// <param name="index">Index of a node.</param>
-        /// <returns>Index of the parent.</returns>
-        private static int GetParent(int index) => (index - 1) / 2;
+		/// <summary>
+		/// Get the index of the parent of a particular node.
+		/// </summary>
+		/// <param name="index">Index of a node.</param>
+		/// <returns>Index of the parent.</returns>
+		private static int GetParent(int index) => (index - 1) / 2;
 
-        /// <summary>
-        /// Get the index of the left child of a particular node.
-        /// </summary>
-        /// <param name="index">Index of a node.</param>
-        /// <returns>Index of the left child.</returns>
-        private static int GetLeftChild(int index) => 2 * index + 1;
+		/// <summary>
+		/// Get the index of the left child of a particular node.
+		/// </summary>
+		/// <param name="index">Index of a node.</param>
+		/// <returns>Index of the left child.</returns>
+		private static int GetLeftChild(int index) => 2 * index + 1;
 
-        /// <summary>
-        /// Get the index of the right child of a particular node.
-        /// </summary>
-        /// <param name="index">Index of a node.</param>
-        /// <returns>Index of the right child.</returns>
-        private static int GetRightChild(int index) => 2 * index + 2;
+		/// <summary>
+		/// Get the index of the right child of a particular node.
+		/// </summary>
+		/// <param name="index">Index of a node.</param>
+		/// <returns>Index of the right child.</returns>
+		private static int GetRightChild(int index) => 2 * index + 2;
 
-        /// <summary>
-        /// Identifies whether an index can be used to access items in heap.
-        /// </summary>
-        /// <param name="index">Item index.</param>
-        /// <returns>True if index is valid, false otherwise.</returns>
-        private bool IsValidIndex(int index) => 0 <= index && index < _list.Count;
+		/// <summary>
+		/// Identifies whether an index can be used to access items in heap.
+		/// </summary>
+		/// <param name="index">Item index.</param>
+		/// <returns>True if index is valid, false otherwise.</returns>
+		private bool IsValidIndex(int index) => 0 <= index && index < _list.Count;
 
-        #endregion
+		#endregion
 
-        #region Public properties
+		#region Public properties
 
-        /// <summary>
-        /// Size of the internal array.
-        /// </summary>
-        public int Capacity => _list.Capacity;
+		/// <summary>
+		/// Size of the internal array.
+		/// </summary>
+		public int Capacity => _list.Capacity;
 
-        #endregion
+		#endregion
 
-        #region Public methods
+		#region Public methods
 
-        /// <summary>
-        /// Add a new item to the heap.
-        /// </summary>
-        /// <param name="item">Item to add.</param>
-        /// <exception cref="ArgumentNullException"></exception>
-        public void Add([NotNull] T item)
-        {
-            if (item == null) throw new ArgumentNullException(nameof(item));
+		/// <summary>
+		/// Add a new item to the heap.
+		/// </summary>
+		/// <param name="item">Item to add.</param>
+		/// <exception cref="ArgumentNullException"></exception>
+		public void Add(T item)
+		{
+			if (item == null) throw new ArgumentNullException(nameof(item));
 
-            _list.Add(item);
+			_list.Add(item);
 
-            var currentIndex = _list.Count - 1;
-            var parentIndex = GetParent(currentIndex);
-            while (currentIndex > 0 && _list[currentIndex].CompareTo(_list[parentIndex]) < 0)
-            {
-                var temp = _list[currentIndex];
-                _list[currentIndex] = _list[parentIndex];
-                _list[parentIndex] = temp;
-                currentIndex = parentIndex;
-                parentIndex = GetParent(parentIndex);
-            }
-        }
+			var currentIndex = _list.Count - 1;
+			var parentIndex = GetParent(currentIndex);
+			while (currentIndex > 0 && _list[currentIndex].CompareTo(_list[parentIndex]) < 0)
+			{
+				var temp = _list[currentIndex];
+				_list[currentIndex] = _list[parentIndex];
+				_list[parentIndex] = temp;
+				currentIndex = parentIndex;
+				parentIndex = GetParent(parentIndex);
+			}
+		}
 
-        /// <summary>
-        /// Remove the least item from the heap.
-        /// </summary>
-        /// <returns>Removed item.</returns>
-        /// <exception cref="InvalidOperationException"></exception>
-        [NotNull]
-        public T RemoveMin()
-        {
-            if (_list.Count == 0) throw new InvalidOperationException();
+		/// <summary>
+		/// Remove the least item from the heap.
+		/// </summary>
+		/// <returns>Removed item.</returns>
+		/// <exception cref="InvalidOperationException"></exception>
+		public T RemoveMin()
+		{
+			if (_list.Count == 0) throw new InvalidOperationException();
 
-            var result = _list[0];
-            _list[0] = _list[_list.Count - 1];
-            _list.RemoveAt(_list.Count - 1);
-            var currentIndex = 0;
-            var leftIndex = GetLeftChild(0);
-            var rightIndex = GetRightChild(0);
+			var result = _list[0];
+			_list[0] = _list[_list.Count - 1];
+			_list.RemoveAt(_list.Count - 1);
+			var currentIndex = 0;
+			var leftIndex = GetLeftChild(0);
+			var rightIndex = GetRightChild(0);
 
-            while (IsValidIndex(leftIndex) || IsValidIndex(rightIndex))
-            {
-                var minIndex = currentIndex;
-                if (IsValidIndex(leftIndex) && _list[leftIndex].CompareTo(_list[minIndex]) < 0)
-                {
-                    minIndex = leftIndex;
-                }
-                if (IsValidIndex(rightIndex) && _list[rightIndex].CompareTo(_list[minIndex]) < 0)
-                {
-                    minIndex = rightIndex;
-                }
+			while (IsValidIndex(leftIndex) || IsValidIndex(rightIndex))
+			{
+				var minIndex = currentIndex;
+				if (IsValidIndex(leftIndex) && _list[leftIndex].CompareTo(_list[minIndex]) < 0) minIndex = leftIndex;
+				if (IsValidIndex(rightIndex) && _list[rightIndex].CompareTo(_list[minIndex]) < 0) minIndex = rightIndex;
 
-                if (minIndex == currentIndex) break;
+				if (minIndex == currentIndex) break;
 
-                var temp = _list[currentIndex];
-                _list[currentIndex] = _list[minIndex];
-                _list[minIndex] = temp;
+				var temp = _list[currentIndex];
+				_list[currentIndex] = _list[minIndex];
+				_list[minIndex] = temp;
 
-                currentIndex = minIndex;
-                leftIndex = GetLeftChild(currentIndex);
-                rightIndex = GetRightChild(currentIndex);
-            }
+				currentIndex = minIndex;
+				leftIndex = GetLeftChild(currentIndex);
+				rightIndex = GetRightChild(currentIndex);
+			}
 
-            return result;
-        }
+			return result;
+		}
 
-        /// <summary>
-        /// Look at the minimum element.
-        /// </summary>
-        /// <returns>Minimum element</returns>
-        /// <exception cref="InvalidOperationException"></exception>
-        [NotNull]
-        public T Min()
-        {
-            if (_list.Count == 0) throw new InvalidOperationException();
-            return _list[0];
-        }
+		/// <summary>
+		/// Look at the minimum element.
+		/// </summary>
+		/// <returns>Minimum element</returns>
+		/// <exception cref="InvalidOperationException"></exception>
+		public T Min()
+		{
+			if (_list.Count == 0) throw new InvalidOperationException();
+			return _list[0];
+		}
 
-        #endregion
+		#endregion
 
-        #region IPriorityQueue
+		#region IPriorityQueue
 
-        /// <summary>
-        /// Number of items in the heap.
-        /// </summary>
-        public int Count => _list.Count;
+		/// <summary>
+		/// Number of items in the heap.
+		/// </summary>
+		public int Count => _list.Count;
 
-        /// <summary>
-        /// Add a new element to the heap.
-        /// </summary>
-        /// <param name="item">Item to add.</param>
-        void IPriorityQueue<T>.Enqueue(T item)
-        {
-            if (item == null) throw new ArgumentNullException(nameof(item));
-            Add(item);
-        }
+		/// <summary>
+		/// Add a new element to the heap.
+		/// </summary>
+		/// <param name="item">Item to add.</param>
+		void IPriorityQueue<T>.Enqueue(T item)
+		{
+			if (item == null) throw new ArgumentNullException(nameof(item));
+			Add(item);
+		}
 
-        /// <summary>
-        /// Remove the least item.
-        /// </summary>
-        /// <returns>Removed item.</returns>
-        T IPriorityQueue<T>.Dequeue()
-        {
-            if (Count == 0) throw new InvalidOperationException();
-            return RemoveMin();
-        }
+		/// <summary>
+		/// Remove the least item.
+		/// </summary>
+		/// <returns>Removed item.</returns>
+		T IPriorityQueue<T>.Dequeue()
+		{
+			if (Count == 0) throw new InvalidOperationException();
+			return RemoveMin();
+		}
 
-        /// <summary>
-        /// Look at the least item.
-        /// </summary>
-        /// <returns>The least item.</returns>
-        T IPriorityQueue<T>.Peek()
-        {
-            if (_list.Count == 0) throw new InvalidOperationException();
-            return _list[0];
-        }
+		/// <summary>
+		/// Look at the least item.
+		/// </summary>
+		/// <returns>The least item.</returns>
+		T IPriorityQueue<T>.Peek()
+		{
+			if (_list.Count == 0) throw new InvalidOperationException();
+			return _list[0];
+		}
 
-        #endregion
+		#endregion
 
-        #region IEnumerable
+		#region IEnumerable
 
-        public IEnumerator<T> GetEnumerator() => _list.GetEnumerator();
+		public IEnumerator<T> GetEnumerator() => _list.GetEnumerator();
 
-        IEnumerator IEnumerable.GetEnumerator() => GetEnumerator();
+		IEnumerator IEnumerable.GetEnumerator() => GetEnumerator();
 
-        #endregion
-    }
+		#endregion
+	}
 }
